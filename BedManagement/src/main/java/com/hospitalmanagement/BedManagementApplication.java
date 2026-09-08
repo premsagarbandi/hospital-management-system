c2b6477f78da0883fc8c28487f3d8f4e97cc893c
PremSagar <premsagarbandi.dev@gmail.com>
2026-09-08 12:29:16 +0530

    Initial commit

---
+++ BedManagement/src/main/java/com/hospitalmanagement/BedManagementApplication.java
@@ -0,0 +1,13 @@
+package com.hospitalmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BedManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BedManagementApplication.class, args);
+	}
+
+}
